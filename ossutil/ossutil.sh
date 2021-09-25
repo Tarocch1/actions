145d1a88c43024da4f63aa794a6fbcbbc5b0dd5c
Tarocch1 <huiben.fang@gmail.com>
2021-09-25 09:18:48 +0800

    feat(ossutil): 判断 ossutil64 是否存在

--- ossutil/ossutil.sh
+++ ossutil/ossutil.sh
@@ -1,7 +1,9 @@
 set -e
 
-wget -P /usr/local/bin http://gosspublic.alicdn.com/ossutil/1.7.7/ossutil64
-chmod 755 /usr/local/bin/ossutil64
+if [ ! -f "/usr/local/bin/ossutil64" ]; then
+  wget -P /usr/local/bin http://gosspublic.alicdn.com/ossutil/1.7.7/ossutil64
+  chmod 755 /usr/local/bin/ossutil64
+fi
 
 ossutil64 config -e ${INPUT_REGION} -i ${INPUT_KEYID} -k ${INPUT_SECRET}
 ossutil64 ${INPUT_ARGS}
