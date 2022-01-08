5c602b6e93c4ff8757e0114f08d3bb7171a800db
Tarocch1 <huiben.fang@gmail.com>
2022-01-09 00:51:25 +0800

    fix(ossutil):

--- ossutil/ossutil.sh
+++ ossutil/ossutil.sh
@@ -1,10 +1,4 @@
 set -e
 
-cd ${PWD}
-
-ls -la
-
-chmod 755 ./ossutil64
-
-./ossutil64 config -e ${INPUT_REGION} -i ${INPUT_KEYID} -k ${INPUT_SECRET}
-./ossutil64 ${INPUT_ARGS}
+ossutil64 config -e ${INPUT_REGION} -i ${INPUT_KEYID} -k ${INPUT_SECRET}
+ossutil64 ${INPUT_ARGS}
