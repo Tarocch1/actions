d6fdc8029dda80d78585841f855fa90d5e610976
Tarocch1 <huiben.fang@gmail.com>
2022-01-09 00:38:10 +0800

    feat(ossutil): bin

--- ossutil/ossutil.sh
+++ ossutil/ossutil.sh
@@ -1,9 +1,6 @@
 set -e
 
-if [ ! -f "/usr/local/bin/ossutil64" ]; then
-  wget -P /usr/local/bin http://gosspublic.alicdn.com/ossutil/1.7.7/ossutil64
-  chmod 755 /usr/local/bin/ossutil64
-fi
+chmod 755 ./ossutil64
 
-ossutil64 config -e ${INPUT_REGION} -i ${INPUT_KEYID} -k ${INPUT_SECRET}
-ossutil64 ${INPUT_ARGS}
+./ossutil64 config -e ${INPUT_REGION} -i ${INPUT_KEYID} -k ${INPUT_SECRET}
+./ossutil64 ${INPUT_ARGS}
